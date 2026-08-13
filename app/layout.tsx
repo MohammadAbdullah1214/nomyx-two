9d651ed864af2d9fb7514aea85d197375664607e
MohammadAbdullah1214 <muhd.abdullah2019@gmail.com>
2026-08-13 17:57:06 +0500

    Added google tag manager (GTM)

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,28 +72,26 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${openSauceSans.variable}`}>
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MXFWRT49');`,
+          }}
+        />
         <script src="https://analytics.ahrefs.com/analytics.js" data-key="NrLNMOqsZRzdvyokHjuxwQ" async></script>
-        {process.env.G_TAG && (
-          <>
-            <script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.G_TAG}`}
-            />
-            <script
-              id="google-analytics"
-              dangerouslySetInnerHTML={{
-                __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${process.env.G_TAG}');
-                `,
-              }}
-            />
-          </>
-        )}
       </head>
       <body className="antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MXFWRT49"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
         <ToastProvider />
       </body>
